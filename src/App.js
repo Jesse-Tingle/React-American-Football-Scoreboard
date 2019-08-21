3830dd171e6644cce5955f20b5c97e5827d83f4f
Jesse-Tingle <jtingle0@gmail.com>
2019-08-21 16:07:21 -0400

    change point value for touchdown

--- src/App.js
+++ src/App.js
@@ -31,11 +31,11 @@ function App() {
       <section className="buttons">
         <div className="homeButtons">
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
-          <button onClick = {() => setLionsScore(lionsScore + 6)} className="homeButtons__touchdown">Home Touchdown</button>
+          <button onClick = {() => setLionsScore(lionsScore + 7)} className="homeButtons__touchdown">Home Touchdown</button>
           <button onClick = {() => setLionsScore(lionsScore + 3)}className="homeButtons__fieldGoal">Home Field Goal</button>
         </div>
         <div className="awayButtons">
-          <button onClick = {() => setTigersScore(tigersScore + 6)}className="awayButtons__touchdown">Away Touchdown</button>
+          <button onClick = {() => setTigersScore(tigersScore + 7)}className="awayButtons__touchdown">Away Touchdown</button>
           <button onClick = {() => setTigersScore(tigersScore + 3)}className="awayButtons__fieldGoal">Away Field Goal</button>
         </div>
       </section>
